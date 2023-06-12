eb36f1428e3650b3cb98042a9c1a1309efde2755
Hardik29 <singhhardik14@gmail.com>
2023-06-12 15:29:48 +0530

    Handled Lecture COntent

--- client/src/scenes/lectures/index.jsx
+++ client/src/scenes/lectures/index.jsx
@@ -11,6 +11,11 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import axios from "axios";
 import { Editor } from '@tinymce/tinymce-react';
+import TextFields from "./TextFields";
+import './index.css'
+
+
+
 
 const Lectures = () => {
     const [title, setTitle] = useState(null);
@@ -22,6 +27,34 @@ const Lectures = () => {
     console.log("LecId=" + LecId);
     const [selectedCourse, setSelectedCourse] = useState(null);
     const [courses, setCourses] = useState([]);
+    const [paraCount, setParaCount] = useState(0);
+    const [arrJSON, setArrJSON] = useState([])
+    const [subParaArr, setSubParaArr] = useState([]);
+
+
+
+    const handleIncrement = () => {
+        setParaCount((paraCount) => paraCount + 1);
+    }
+
+    
+    const handleDecrement = () => {
+        setParaCount((paraCount) => paraCount - 1);
+        arrJSON.pop();
+    }
+
+    useEffect(() => {
+        const updatedSubParaArr = [];
+        for (let i = 0; i < paraCount; i++) {
+            updatedSubParaArr.push(
+                <div key={i}>
+                    <TextFields memberIndex={i} arr={arrJSON} />
+                </div>  
+            );
+        }
+        setSubParaArr(updatedSubParaArr);
+    }, [paraCount, arrJSON]);
+
 
     const fetchLecture = async () => {
         await axios.get(`${process.env.REACT_APP_API_URL}/client/lecture?id=${LecId}`)
@@ -59,11 +92,12 @@ const Lectures = () => {
     const addNewLec = () => {
         if (editorRef.current) {
             console.log(title)
+            console.log(editorRef.current.getContent())
             // axios posst request to serve
             axios.post(`${process.env.REACT_APP_API_URL}/client/lecture/add`, {
                 no: 1,
                 title: title,
-                content: editorRef.current.getContent(),
+                content: [editorRef.current.getContent()],
                 courseId: selectedCourse
             })
                 .then(function (response) {
@@ -92,52 +126,58 @@ const Lectures = () => {
 
     return (
         <>
-            <TextField id="outlined-basic" label={title ? title : "Enter Title"} variant="outlined" onChange={(event) => setTitle(event.target.value)} />
-            <FormControl sx={{ m: 1, minWidth: 320 }}>
-                <InputLabel id="demo-simple-select-helper-label">Select course</InputLabel>
-                <Select
-                    labelId="demo-simple-select-helper-label"
-                    id="demo-simple-select-helper"
-                    value={selectedCourse}
-                    label="Select a Course"
-                    onChange={(e) => setSelectedCourse(e.target.value) && console.log(e.target.value)}
-                >
-                    {courses.map((course) => (
-                        <MenuItem value={course._id}>{course.courseName}</MenuItem>
-                    ))}
-                    {/* <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem> */}
-                </Select>
-            </FormControl>
-            <h2>Content</h2>
-            <Editor apiKey='18njo3cex5ijqgdwlkqewqxzo8xxvuiln9hwtasdb5muxnth'
-                onInit={(evt, editor) => editorRef.current = editor}
-                initialValue={content ? content : "<p>Type here !!</p>"}
-                init={{
-                    selector: 'textarea#local-upload',
-                    plugins: 'image code',
-                    toolbar: 'undo redo | image code| formatselect | ' +
-                        'bold italic backcolor | alignleft aligncenter ' +
-                        'alignright alignjustify | bullist numlist outdent indent | ' +
-                        'removeformat | help',
-
-                    /* without images_upload_url set, Upload tab won't show up*/
-                    images_upload_url: 'postAcceptor.php',
-
-                    /* we override default upload handler to simulate successful upload*/
-                    images_upload_handler: function (blobInfo, success, failure) {
-                        setTimeout(function () {
-                            /* no matter what you upload, we will turn it into TinyMCE logo :)*/
-                            success('http://moxiecode.cachefly.net/tinymce/v9/images/logo.png');
-                        }, 2000);
-                    },
-                    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-                }}
-            />
+            <div className="index">
+                <TextField id="outlined-basic" label={title ? title : "Enter Title"} variant="outlined" onChange={(event) => setTitle(event.target.value)} />
+                <FormControl sx={{ m: 1, minWidth: 320 }}>
+                    <InputLabel id="demo-simple-select-helper-label">Select course</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        value={selectedCourse}
+                        label="Select a Course"
+                        onChange={(e) => setSelectedCourse(e.target.value) && console.log(e.target.value)}
+                    >
+                        {courses.map((course) => (
+                            <MenuItem value={course._id}>{course.courseName}</MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </div>
+            <div className="textField">
+                {subParaArr}
+                {console.log(paraCount)}
+                <button onClick={handleIncrement}>Add SubPara</button>
+                {paraCount !== 0 && <button onClick={handleDecrement}>Delete SubPara</button>}
+            </div>
             <button onClick={LecId ? updateLecture : addNewLec}>{LecId ? "Update Lecture" : "Add Lecture"}</button>
         </>
     );
 };
 
 export default Lectures;
+
+
+// {/* <Editor apiKey='18njo3cex5ijqgdwlkqewqxzo8xxvuiln9hwtasdb5muxnth'
+// onInit={(evt, editor) => editorRef.current = editor}
+// initialValue={content ? content : "<p>Type here !!</p>"}
+// init={{
+//     selector: 'textarea#local-upload',
+//     plugins: 'image code',
+//     toolbar: 'undo redo | image code| formatselect | ' +
+//         'bold italic backcolor | alignleft aligncenter ' +
+//         'alignright alignjustify | bullist numlist outdent indent | ' +
+//         'removeformat | help',
+
+//     /* without images_upload_url set, Upload tab won't show up*/
+//     images_upload_url: 'postAcceptor.php',
+
+//     /* we override default upload handler to simulate successful upload*/
+//     images_upload_handler: function (blobInfo, success, failure) {
+//         setTimeout(function () {
+//             /* no matter what you upload, we will turn it into TinyMCE logo :)*/
+//             success('http://moxiecode.cachefly.net/tinymce/v9/images/logo.png');
+//         }, 2000);
+//     },
+//     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+// }}
+// /> */}
