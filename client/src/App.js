853410fabb6976cdd048cbc0e8fa35b8ee9ea038
Hardik29 <singhhardik14@gmail.com>
2023-08-22 18:10:37 +0530

    More Refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
 import Layout from "scenes/layout";
-import Dashboard from "scenes/dashboard";
 import Lectures from "scenes/lectures";
 import Courses from "scenes/courses";
 import Users from "scenes/users";
@@ -14,7 +13,6 @@ import Assignments from "scenes/assignments";
 import AddAssignment from "scenes/addAssignment";
 import Enrollments from "scenes/enrollments";
 import Login from "scenes/login";
-import Admin from "scenes/admin";
 import RequireAuth from "./components/RequireAuth";
 import AddCourse from "scenes/addCourse";
 import AddUser from "scenes/addUser";
@@ -32,13 +30,11 @@ function App() {
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          {/* <Layout /> */}
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route element={<RequireAuth />} >
               <Route element={<Layout />}>
                 <Route path="/" element={<Navigate to="/courses" replace />} />
-                <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/lectures" element={<Lectures />} />
                 <Route path="/lecture" element={<Lectures />} /> // route for lecture details
                 <Route path="/courses" element={<Courses />} />
@@ -52,7 +48,6 @@ function App() {
                 <Route path="/minilecdetails" element={<MiniLecDetails />} />
                 <Route path="/assignments" element={<Assignments />} />
                 <Route path="/assignments/add" element={<AddAssignment />} />
-                <Route path="/admin" element={<Admin />} />
               </Route>
             </Route>
           </Routes>
--- client/src/scenes/lectures/index.jsx
+++ client/src/scenes/lectures/index.jsx
@@ -176,8 +176,6 @@ const Lectures = () => {
 
   useEffect(() => {
     fetchCourses();
-    setAlertLecAdded();
-    setAlertLecAddedErr();
   }, []);
 
   useEffect(() => {
